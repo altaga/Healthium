ae4ffd1d049251ac6d65a1ecc1bc8a20b72b5269
Altaga <v.a.i@hotmail.com>
2021-08-30 01:27:02 -0500

    commit

---
+++ Arduino_Code/Arduino_Code.ino
@@ -0,0 +1,183 @@
+#include <M5Core2.h>
+#include <Adafruit_NeoPixel.h>
+#include <Wire.h>
+#include <Adafruit_MLX90614.h>
+#include "MQ135.h"
+
+#define PIN       25 // M5Stack NeoPixel Pin
+#define NUMPIXELS 10 // M5 Neopixel Number
+#define RXD2 13
+#define TXD2 14
+#define LoraRate 60000
+#define RZERO 76.63
+
+Adafruit_NeoPixel M5pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_MLX90614 mlx = Adafruit_MLX90614();
+MQ135 gasSensor = MQ135(36);
+
+unsigned long inter1;
+unsigned long inter2;
+unsigned long trig;
+bool flag = false;
+float batVoltage;
+int batPercentage;
+int gSensor = 1;
+int tempSensor;
+
+void setup() {
+  M5.begin();
+  SD.begin();
+  M5pixels.begin();
+  setNeoPixel(0, 0, 0);
+  Serial.begin(115200);
+  M5.Lcd.fillScreen(WHITE);
+  M5.Lcd.drawJpgFile(SD, "/cover.jpg", 0, 0);
+  M5.Lcd.setTextColor(BLACK);
+  M5.Lcd.setTextSize(2);
+  M5.Lcd.setCursor(150, 12);
+  M5.Lcd.println(" o");
+  M5.Lcd.setTextSize(5);
+  M5.Lcd.setCursor(83, 100);
+  M5.Lcd.println(String(int(gasSensor.getPPM())) + " ppm");
+  Serial2.begin(115200, SERIAL_8N1, RXD2, TXD2);
+  mlx.begin();
+  inter1 = millis();
+  inter2 = millis();
+  trig = millis();
+}
+
+void loop() {
+  if (Serial2.available()) {
+    String state = Serial2.readString();
+    if (state == "ok") {
+      M5.Lcd.setTextSize(4);
+      M5.Lcd.setTextColor(WHITE);
+      M5.Lcd.setCursor(83, 182);
+      M5.Lcd.println(".....");
+      M5.Lcd.setCursor(83, 182);
+      M5.Lcd.println("Error");
+      M5.Lcd.setTextColor(GREEN);
+      M5.Lcd.setCursor(83, 182);
+      M5.Lcd.println("Online");
+      M5.Lcd.setTextColor(BLACK);
+      M5.Lcd.setTextSize(5);
+      flag = false;
+    }
+    if (state == "sending") {
+      M5.Lcd.setTextSize(4);
+      M5.Lcd.setTextColor(WHITE);
+      M5.Lcd.setCursor(83, 182);
+      M5.Lcd.println("Online");
+      M5.Lcd.setCursor(83, 182);
+      M5.Lcd.println("Error");
+      M5.Lcd.setTextColor(BLACK);
+      M5.Lcd.setCursor(83, 182);
+      M5.Lcd.println(".....");
+      M5.Lcd.setTextSize(5);
+      flag = false;
+    }
+  }
+  gSensor = gasSensor.getPPM();
+  tempSensor = int(correlation(mlx.readAmbientTempC(), mlx.readObjectTempC()));
+  dispNeoPixel(correlation(mlx.readAmbientTempC(), mlx.readObjectTempC()));
+  batVoltage = M5.Axp.GetBatVoltage();
+  batPercentage = int(( batVoltage < 3.2 ) ? 0 : ( batVoltage - 3.2 ) * 100);
+  if ((millis() - inter1) > LoraRate) {
+    Serial2.println(String(gSensor) + "," + String(correlation(mlx.readAmbientTempC(), mlx.readObjectTempC())));
+    flag = true;
+    trig = millis();
+    inter1 = millis();
+  }
+  if ((millis() - inter2) > 1000) {
+    disp(gSensor, tempSensor, batPercentage);
+    inter2 = millis();
+  }
+  if ((millis() - trig) > 10000 && flag) {
+    M5.Lcd.setTextSize(4);
+    M5.Lcd.setTextColor(WHITE);
+    M5.Lcd.setCursor(83, 182);
+    M5.Lcd.println(".....");
+    M5.Lcd.setCursor(83, 182);
+    M5.Lcd.println("Online");
+    M5.Lcd.setTextColor(RED);
+    M5.Lcd.setCursor(83, 182);
+    M5.Lcd.println("Error");
+    M5.Lcd.setTextColor(BLACK);
+    M5.Lcd.setTextSize(5);
+    flag = false;
+  }
+}
+
+void dispNeoPixel(float temp) {
+  if (temp > 38) {
+    setNeoPixel(200, 0, 0);
+  }
+  else if (temp <= 38 && temp > 37) {
+    setNeoPixel(200, 180, 0);
+  }
+  else if (temp <= 37 && temp > 36) {
+    setNeoPixel(0, 200, 0);
+  }
+  else if (temp <= 36 && temp > 35) {
+    setNeoPixel(0, 200, 180);
+  }
+  else if (temp <= 35 && temp > 34) {
+    setNeoPixel(0, 0, 200);
+  }
+  else {
+    setNeoPixel(0, 0, 0);
+  }
+}
+
+void setNeoPixel(int R, int G, int B) {
+  M5pixels.clear();
+  for (int i = 0; i < NUMPIXELS; i++) {
+    M5pixels.setPixelColor(i, M5pixels.Color(R, G, B));
+  }
+  M5pixels.show();
+  delay(10);
+}
+
+void disp(int gas, int temp, int battery) {
+  static int gasMem;
+  static int tempMem;
+  static int batMem;
+  if (gasMem != gas) {
+    M5.Lcd.setTextColor(WHITE);
+    M5.Lcd.setCursor(83, 100);
+    M5.Lcd.println(String(gasMem) + " ppm");
+    M5.Lcd.setTextColor(BLACK);
+    gasMem = gas;
+    M5.Lcd.setCursor(83, 100);
+    M5.Lcd.println(String(gas) + " ppm");
+  }
+  if (tempMem != temp) {
+    M5.Lcd.setTextColor(WHITE);
+    M5.Lcd.setCursor(83, 20);
+    M5.Lcd.println(String(tempMem));
+    M5.Lcd.setTextColor(BLACK);
+    tempMem = temp;
+    M5.Lcd.setCursor(83, 20);
+    M5.Lcd.println(String(temp) + " C");
+  }
+  if (batMem != battery) {
+    M5.Lcd.setTextSize(2);
+    M5.Lcd.setTextColor(WHITE);
+    M5.Lcd.setCursor(264, 4);
+    M5.Lcd.println(String(batMem) + "%");
+    M5.Lcd.setTextColor(BLACK);
+    batMem = battery;
+    M5.Lcd.setCursor(264, 4);
+    M5.Lcd.println(String(battery) + "%");
+    M5.Lcd.setTextSize(5);
+  }
+  delay(1000);
+}
+
+float correlation(float amb, float skin) {
+  if (skin > 27 && skin < 36) {
+    float realTemp = 0.71429 * skin - 0.35714 * amb + 23.14286;
+    return realTemp;
+  }
+  return skin;
+}
---
+++ Helium LoraWAN/main.py
@@ -0,0 +1,42 @@
+from network import LoRa
+import socket
+import time
+from machine import UART
+from config import dev_eui, app_eui, app_key
+
+uart = UART(1, baudrate=115200)
+data = None
+
+lora = LoRa(mode=LoRa.LORAWAN, region=LoRa.US915)
+
+# imported from config.py dev_eui, app_eui, app_key
+
+for i in range(0,8):
+    lora.remove_channel(i)
+for i in range(16,65):
+    lora.remove_channel(i)
+for i in range(66,72):
+    lora.remove_channel(i)
+
+lora.join(activation=LoRa.OTAA, auth=(dev_eui, app_eui, app_key), timeout=0)
+
+while not lora.has_joined():
+    time.sleep(2.5)
+    uart.write("sending")
+    print('Not yet joined...')
+
+print('Joined')
+uart.write("ok")
+s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
+s.setsockopt(socket.SOL_LORA, socket.SO_DR, 1)
+
+while 1:
+    data = uart.readline() # read up to 5 bytes}
+    if data == None:
+        ...
+    else:
+        uart.write("sending")
+        s.setblocking(True)
+        s.send(data)
+        s.setblocking(False)
+        uart.write("ok")
